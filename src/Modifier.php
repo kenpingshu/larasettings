affcdefb07930be72348b8f291f9eb8ecd9c80b4
kenpingshu <knel.tw@gmail.com>
2022-03-03 19:33:37 +0800

    Initial commit

---
+++ src/Modifier.php
@@ -0,0 +1,9 @@
+<?php
+
+class Modifier
+{
+    public function update($name, $value)
+    {
+
+    }
+}
---
+++ src/Settings.php
@@ -0,0 +1,16 @@
+<?php
+
+class Settings
+{
+
+    public function init($belongsId)
+    {
+
+    }
+
+    public function get(int $id)
+    {
+
+    }
+
+}
